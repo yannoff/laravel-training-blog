da12b911e0c9d033fa360d4f17ef2b5d8207eec1
Yannoff <yannoff.it@gmail.com>
2021-12-13 10:07:53 +0100

    Use `yannoff/lumiere-utils`

--- database/migrations/2021_10_17_111244_create_topics_table.php
+++ database/migrations/2021_10_17_111244_create_topics_table.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use App\Database\Schema\Utils;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Yannoff\Lumiere\Utils\Database\BluePrintUtils as Utils;
 
 class CreateTopicsTable extends Migration
 {
--- database/migrations/2021_10_20_111252_create_tags_table.php
+++ database/migrations/2021_10_20_111252_create_tags_table.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Database\Schema\Utils;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Yannoff\Lumiere\Utils\Database\BluePrintUtils as Utils;
 
 class CreateTagsTable extends Migration
 {
